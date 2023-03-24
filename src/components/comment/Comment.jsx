47cca62fadb71ba169ea4257b2ba74e3fa9871a6
Shirsho Dipto <shirshodipto@gmail.com>
2023-03-24 21:07:02 +0600

    Add comment like functionality

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -2,17 +2,112 @@ import "./comment.css";
 import Replies from "../replies/Replies";
 import CommentContent from "../commentContent/CommentContent";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Comment({ currentUser, post, comment }) {
   const serverUri = process.env.REACT_APP_PROXY;
+  const params = useParams();
 
   const [commentState, setCommentState] = useState({
     comment: comment,
     isLiked: {},
   });
 
-  function handleLike() {
-    console.log("handling like. ");
+  async function deleteLike() {
+    const res = await fetch(
+      `${serverUri}/posts/${params.postId}/comments/${commentState.comment._id}/likes/${commentState.isLiked.commentLike._id}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${currentUser.token}`,
+        },
+      }
+    );
+
+    if (!res.ok) {
+      return "Something bad happened. Error Ocurred. ";
+    }
+  }
+
+  async function addLike() {
+    const res = await fetch(
+      `${serverUri}/posts/${params.postId}/comments/${commentState.comment._id}/likes`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${currentUser.token}`,
+        },
+      }
+    );
+
+    if (!res.ok) {
+      return "Something bad happened. Error Ocurred. ";
+    }
+    const like = await res.json();
+    return like;
+  }
+
+  async function delCommentLike() {
+    const res = await fetch(
+      `${serverUri}/posts/${params.postId}/comments/${commentState.comment._id}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${currentUser.token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          updateType: "like",
+          numLikes: commentState.comment.numLikes - 1,
+        }),
+      }
+    );
+
+    if (!res.ok) {
+      console.log(await res.json());
+      return "Something bad happened.";
+    }
+
+    const resData = await res.json();
+    return resData.comment;
+  }
+
+  async function addLikeToComment() {
+    const res = await fetch(
+      `${serverUri}/posts/${params.postId}/comments/${commentState.comment._id}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${currentUser.token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          updateType: "like",
+          numLikes: commentState.comment.numLikes + 1,
+        }),
+      }
+    );
+
+    if (!res.ok) {
+      console.log(await res.json());
+      return "Something bad happened.";
+    }
+
+    const resData = await res.json();
+    return resData.comment;
+  }
+
+  async function handleLike() {
+    if (!currentUser) {
+      return alert("Log in to Like. ");
+    }
+    if (Object.keys(commentState.isLiked).length > 0) {
+      const responses = await Promise.all([deleteLike(), delCommentLike()]);
+      return setCommentState({ comment: responses[1], isLiked: {} });
+    } else {
+      const responses = await Promise.all([addLike(), addLikeToComment()]);
+      return setCommentState({ comment: responses[1], isLiked: responses[0] });
+    }
   }
 
   useEffect(() => {
